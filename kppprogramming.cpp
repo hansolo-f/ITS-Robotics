abe730057cff65ff0bbe1994e3afe63a3d8a48b5
hansolo-f <55346278+hansolo-f@users.noreply.github.com>
2022-10-12 19:34:38 +0700

    Add files via upload

---
+++ kppprogramming.cpp
@@ -0,0 +1,46 @@
+// NAMA		: FARHAN ARDIA NASHWAN
+// NRP		: 5025221308
+// Jurusan	: TEKNIK INFORMATIKA
+
+#include <iostream>
+#include <cmath>
+
+#define GRAVITASI 10 //10 m/s^2
+#define START_PENGUKURAN 1 //pengukuran dimulai dari 1 meter
+#define SUDUT 45 //sudut elevasi tembakan
+
+float mencari_V0(float Vt, float speed_loss)
+{
+	float V0 = Vt - speed_loss;
+  	return V0;
+}
+
+float speed_dgn_loss(float x)
+{
+	float loss;
+	if (x >= 2 && x <= 11) {
+		loss = 1;
+	}
+	else if (x >= 14 && x <= 23) {
+		loss = 3;
+	}
+	else if (x >= 26 && x <= 30) {
+		loss = 5;
+	}
+    return loss;
+}
+
+int main() {
+	float input;
+	float loss = speed_dgn_loss(input);
+	float V0 = mencari_V0(input, loss);
+	int jarak;
+
+  	std::cin >> input;
+
+	jarak = V0*V0 * sin(2*SUDUT*3.14/180) * GRAVITASI;
+	double tangensial = sqrt(jarak * GRAVITASI / sin(2 * SUDUT * 3.14 / 180)) + loss;
+
+  	std::cout << jarak << " " << tangensial << std::endl;
+    return 0;
+}
